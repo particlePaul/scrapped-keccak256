9859ab053eed02d7969a0decc6cdd437b52444fd
Particle Paul <particlePaul@users.noreply.github.com>
2017-06-27 01:10:19 -0400

    Update keccak256.php

--- src/keccak256.php
+++ src/keccak256.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Keccak256;
+
 
 const ALGO_NAME = 'keccak256';
 
-function hash($algo, $data, $raw_output = false) {
+
+function hash(string $algo, string $data, bool $raw_output = false): string {
     if ($algo === ALGO_NAME) {
         return keccak256($data, $raw_output);
     } else {
@@ -12,6 +16,12 @@ function hash($algo, $data, $raw_output = false) {
     }
 }
 
-function keccak256($data, $raw_output = false) {
-    die('not implemented');
+
+function keccak256(string $data, bool $raw_output = false): string {
+    if ($data === '') {
+        $hex = '5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02';
+        return $raw_output ? pack('H*', $hex) : $hex;
+    }
+
+    throw new \UnexpectedValueException('not implemented');
 }
