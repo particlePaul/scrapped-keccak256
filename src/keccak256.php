6ee5bdc43b31e95a7ba274f8ee33b1ea1bd7a214
Particle Paul <particlePaul@users.noreply.github.com>
2017-06-27 12:31:38 -0400

    try to understand this algorithm

--- src/keccak256.php
+++ src/keccak256.php
@@ -10,6 +10,14 @@ function keccak256(string $data, ?bool $raw_output = false): string {
 }
 
 class Keccak256 {
+    // Keccak algorithim parameters
+    const CAPACITY_BITS = 1088;
+    const RATE_BITS = 512;
+    const RATE_BYTES = RATE_BITS / 8;
+    const LENGTH_BITS = 256;
+    const LENGTH_BYTES = LENGTH_BITS / 8;
+    const LANES = 5 * 5;
+
     // Some known input-output pairs.
     const testVectors = [
         '' =>
@@ -25,7 +33,7 @@ class Keccak256 {
     // Wraps the implementation (which use arrays) in PHP's traditional string hashing interface.    
     static function hash(string $data, ?bool $raw_output = false): string {
         $dataBytes = hexToBytes($data);
-        $resultBytes = Keccak256::keccak256($dataBytes);
+        $resultBytes = Keccak256::keccak_sponge_r1088_f128_d256($dataBytes);
 
         if ($raw_output) {
             $resultRawString = \pack('C*', $resultBytes->toArray());
@@ -40,27 +48,32 @@ class Keccak256 {
     // http://keccak.noekeon.org/specs_summary.html
     // https://github.com/emn178/js-sha3/blob/master/src/sha3.js
     // http://www.movable-type.co.uk/scripts/sha3.html
+    // https://github.com/gvanas/KeccakCodePackage/blob/master/Standalone/CompactFIPS202-Python/CompactFIPS202.py
     
-    protected static function keccak256(\SplFixedArray $value): \SplFixedArray {
-        $result = new \SplFixedArray(256 / 8);
+    protected static function keccak_sponge_c1088_r512_l256(\SplFixedArray $value): \SplFixedArray {
+        $result = new \SplFixedArray(Keccak256::LENGTH_BYTES);
         
-        // 5 x 5 lanes of 64 bits
-        $state = \SplFixedArray::fromArray([
-            \SplFixedArray::fromArray([new Uint64, new Uint64, new Uint64, new Uint64, new Uint64]),
-            \SplFixedArray::fromArray([new Uint64, new Uint64, new Uint64, new Uint64, new Uint64]),
-            \SplFixedArray::fromArray([new Uint64, new Uint64, new Uint64, new Uint64, new Uint64]),
-            \SplFixedArray::fromArray([new Uint64, new Uint64, new Uint64, new Uint64, new Uint64]),
-            \SplFixedArray::fromArray([new Uint64, new Uint64, new Uint64, new Uint64, new Uint64])
-        ]);
+        // State is 1600 bits group into 5x5 lanes of 64 bits each.
+        $state = new \SplFixedArray(LANES);
+        for ($i = 0; $i < LANES; $i++) {
+            $state[$i] = new Uint64;
+        }
 
-        $state[0][0]->rot(2)->xor($state[1][1]);
+        // Break input into RATE (512 bit) size blocks.
+        
+        // Xor each block into the first RATE (512 bit) of $state (treating lanes as contiguous)
+
+        // at the end of each full block, F-up the state.
+        
+        // xor the algorithm specifier suffix into the state at index input length % RATE + 1.
+
 
         throw new \Exception('not implemented');
 
         return $result;
     }
 
-    protected static function keccak256_f(\SplFixedArray $state): void {
+    protected static function keccak_f_b1600(\SplFixedArray $state): void {
        throw new \Exception('not implemented');
     }
 }
@@ -70,6 +83,7 @@ class Keccak256 {
 
 // A minimal byte-wise Uint64 implementing only the operators required for Keccak,
 // because we can't assume we're running on 64-bit PHP.
+// https://github.com/gvanas/KeccakCodePackage/blob/master/Standalone/CompactFIPS202-Python/CompactFIPS202.py
 class Uint64 {
     function __construct() {
         $this->bytes = new \SplFixedArray(8);
@@ -98,9 +112,9 @@ class Uint64 {
         return $result;
     }
 
-    function rot(int $distance): Uint64 {
+    function rotl(int $distance): Uint64 {
         if ($distance < 0) {
-            throw new \UnexpectedValueException('Uint64:rot(...) argument must not be negative');
+            throw new \UnexpectedValueException('Uint64:rotl(...) argument must not be negative');
         }
 
         $bytes = $this->toBytes();
@@ -121,14 +135,14 @@ class Uint64 {
 
         if ($remaining > 0) {
             $tmp = $bytes[0];
-            $bytes[0] = 0xFF & (($bytes[0] >> $remaining) | ($bytes[7] << (8 - $remaining)));
-            $bytes[7] = 0xFF & (($bytes[7] >> $remaining) | ($bytes[6] << (8 - $remaining)));
-            $bytes[6] = 0xFF & (($bytes[6] >> $remaining) | ($bytes[5] << (8 - $remaining)));
-            $bytes[5] = 0xFF & (($bytes[5] >> $remaining) | ($bytes[4] << (8 - $remaining)));
-            $bytes[4] = 0xFF & (($bytes[4] >> $remaining) | ($bytes[3] << (8 - $remaining)));
-            $bytes[3] = 0xFF & (($bytes[3] >> $remaining) | ($bytes[2] << (8 - $remaining)));
-            $bytes[2] = 0xFF & (($bytes[2] >> $remaining) | ($bytes[1] << (8 - $remaining)));
-            $bytes[1] = 0xFF & (($bytes[1] >> $remaining) | (     $tmp << (8 - $remaining)));
+            $bytes[0] = 0xFF & (($bytes[0] << $remaining) | ($bytes[7] >> (8 - $remaining)));
+            $bytes[7] = 0xFF & (($bytes[7] << $remaining) | ($bytes[6] >> (8 - $remaining)));
+            $bytes[6] = 0xFF & (($bytes[6] << $remaining) | ($bytes[5] >> (8 - $remaining)));
+            $bytes[5] = 0xFF & (($bytes[5] << $remaining) | ($bytes[4] >> (8 - $remaining)));
+            $bytes[4] = 0xFF & (($bytes[4] << $remaining) | ($bytes[3] >> (8 - $remaining)));
+            $bytes[3] = 0xFF & (($bytes[3] << $remaining) | ($bytes[2] >> (8 - $remaining)));
+            $bytes[2] = 0xFF & (($bytes[2] << $remaining) | ($bytes[1] >> (8 - $remaining)));
+            $bytes[1] = 0xFF & (($bytes[1] << $remaining) | (     $tmp >> (8 - $remaining)));
             $remaining = 0;
         }
 
@@ -154,11 +168,12 @@ function hexToBytes(string $data): \SplFixedArray {
 function dump($x, $label = "") { echo bytesToHex($x->toBytes()) . " $label\n"; }
 $x = Uint64::fromBytes(hexToBytes('0123456789abcdef'));
 dump($x, 'should be the hex alphabet');
-dump($x->xor($x->rot(1)->rot(1)->rot(12)->rot(3)->rot(21)->rot(26)), 'should be zeroed out');
+dump($x->xor($x->rotl(1)->rotl(1)->rotl(12)->rotl(3)->rotl(21)->rotl(26)), 'should be zeroed out');
 
 echo "\n\n\n";
 
 // Assert that the test vectors produce the correct results.
 foreach (Keccak256::testVectors as $input => $expected) {
+    echo "Testing $input...\n";
     assert($expected === keccak256($input, false), new \Exception('Keccak256 test vector failed'));
 }
